970c7c4921b40cc9866d18be702a7cd63058e2fb
Derek Grayless <grayledw@rose-hulman.edu>
2020-12-17 18:43:07 -0500

    connected game board; need to fix rows being aligned horizontally on iPhone - not arranged in vertical stack view

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
+    // can not rely on the buttons being in any particular order
+    @IBOutlet var gameBoardButtons: [UIButton]!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
 
     @IBAction func pressedNewGame(_ sender: Any) {
         print("New Game")
+        updateView()
+    }
+    
+    @IBAction func pressedGameBoardButton(_ sender: Any) {
+        let button = sender as! UIButton
+        print(button.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        
     }
 }
 
